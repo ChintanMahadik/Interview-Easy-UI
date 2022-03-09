2dec7e61e75b6d3a82a15122a148d33ebdb4bb82
Chintan <chintanmahadik@gmail.com>
2022-03-09 22:13:50 +0530

    Adding theme and image change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-{path:'',component:HomeComponent}
+{path:'',component:HomeComponent},
+{path:'footer', component:FooterComponent}
 ];
 
 @NgModule({
